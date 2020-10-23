627d032b9b9d22e97f299e47a07b131e4c969d16
Shevchenko Elena <elena.kadru@gmail.com>
2020-10-23 17:33:19 +0300

    added hw01 with additional variable

--- contacts.js
+++ contacts.js
@@ -1,29 +1,30 @@
 const fs = require('fs');
+const fsp = require('fs').promises;
 const path = require('path');
 
 const contactsPath = path.join(__dirname, "./db/contacts.json");
 
 // TODO: задокументировать каждую функцию
 async function listContacts() {
-    const data = await fs.promises.readFile(contactsPath, "utf8");
+    const data = await fsp.readFile(contactsPath, "utf8");
     console.table(JSON.parse(data));
 }
 
 async function getContactById(contactId) {
-    const data = await fs.promises.readFile(contactsPath, "utf8");
+    const data = await fsp.readFile(contactsPath, "utf8");
     const findedData = await (JSON.parse(data).find(user => user.id === contactId));
     console.log(findedData);
 }
 
 async function removeContact(contactId) {
-    const data = await fs.promises.readFile(contactsPath, "utf8");
+    const data = await fsp.readFile(contactsPath, "utf8");
     const arr = await JSON.parse(data);
     if (arr.find(user => user.id === contactId)) {
         const filtredData = await (arr.filter(user => user.id !== contactId));
-    await fs.promises.writeFile(contactsPath, JSON.stringify(filtredData), err => {
+        await fsp.writeFile(contactsPath, JSON.stringify(filtredData), err => {
         if (err) { console.warn(err) }
     });
-    const newData = await fs.promises.readFile(contactsPath, "utf8");
+        const newData = await fsp.readFile(contactsPath, "utf8");
     console.table(JSON.parse(newData));        
     } else {
         console.log(`Contact with id=${contactId} does not exist`); 
@@ -31,15 +32,15 @@ async function removeContact(contactId) {
 }
 
 async function addContact(name, email, phone) {
-    const data = await fs.promises.readFile(contactsPath, "utf8");
+    const data = await fsp.readFile(contactsPath, "utf8");
     const arr = await JSON.parse(data);
     const id = arr[arr.length - 1]['id'] + 1;
     const newContact = { id, name, email, phone };
     const contactsWithNew = [...JSON.parse(data), newContact];
-    await fs.promises.writeFile(contactsPath, JSON.stringify(contactsWithNew), err => {
+    await fsp.writeFile(contactsPath, JSON.stringify(contactsWithNew), err => {
         if (err) { console.warn(err) }
     });
-    const newData = await fs.promises.readFile(contactsPath, "utf8");
+    const newData = await fsp.readFile(contactsPath, "utf8");
     console.table(JSON.parse(newData));
 }
 
